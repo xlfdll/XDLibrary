94f7fa02135cc5ba3de07912808a45cd7b0a2581
xlfdll <xlfdll@gmail.com>
2019-01-02 19:21:22 -0800

    Removed redundant assembly version prefix string in AssemblyMetadata class

--- Xlfdll.Core/Diagnostics/AssemblyMetadata.cs
+++ Xlfdll.Core/Diagnostics/AssemblyMetadata.cs
@@ -4,119 +4,119 @@ using System.Reflection;
 
 namespace Xlfdll.Diagnostics
 {
-	public class AssemblyMetadata
-	{
-		public AssemblyMetadata(String path)
-		{
-			this.AssemblyObject = Assembly.LoadFile(path);
-		}
+    public class AssemblyMetadata
+    {
+        public AssemblyMetadata(String path)
+        {
+            this.AssemblyObject = Assembly.LoadFile(path);
+        }
 
-		public AssemblyMetadata(Assembly assembly)
-		{
-			this.AssemblyObject = assembly;
-		}
+        public AssemblyMetadata(Assembly assembly)
+        {
+            this.AssemblyObject = assembly;
+        }
 
-		public Assembly AssemblyObject { get; }
+        public Assembly AssemblyObject { get; }
 
-		public String AssemblyLocation => this.AssemblyObject.Location;
+        public String AssemblyLocation => this.AssemblyObject.Location;
 
-		#region Assembly Internal Name Accessors
+        #region Assembly Internal Name Accessors
 
-		public String AssemblyInternalFileName => Path.GetFileName(this.AssemblyObject.Location);
-		public String AssemblyInternalName => Path.GetFileNameWithoutExtension(this.AssemblyObject.Location);
+        public String AssemblyInternalFileName => Path.GetFileName(this.AssemblyObject.Location);
+        public String AssemblyInternalName => Path.GetFileNameWithoutExtension(this.AssemblyObject.Location);
 
-		#endregion
+        #endregion
 
-		#region Assembly Attribute Accessors
+        #region Assembly Attribute Accessors
 
-		public String AssemblyTitle
-		{
-			get
-			{
-				Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+        public String AssemblyTitle
+        {
+            get
+            {
+                Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
 
-				if (attributes.Length > 0)
-				{
-					AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
 
-					if (titleAttribute.Title != String.Empty)
-					{
-						return titleAttribute.Title;
-					}
-				}
+                    if (titleAttribute.Title != String.Empty)
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
 
-				return Path.GetFileNameWithoutExtension(this.AssemblyObject.CodeBase);
-			}
-		}
+                return Path.GetFileNameWithoutExtension(this.AssemblyObject.CodeBase);
+            }
+        }
 
-		public String AssemblyDescription
-		{
-			get
-			{
-				Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+        public String AssemblyDescription
+        {
+            get
+            {
+                Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
 
-				if (attributes.Length == 0)
-				{
-					return String.Empty;
-				}
+                if (attributes.Length == 0)
+                {
+                    return String.Empty;
+                }
 
-				return ((AssemblyDescriptionAttribute)attributes[0]).Description;
-			}
-		}
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
 
-		public String AssemblyVersion
-		{
-			get
-			{
-				return "Version " + this.AssemblyObject.GetName().Version.ToString();
-			}
-		}
+        public String AssemblyVersion
+        {
+            get
+            {
+                return this.AssemblyObject.GetName().Version.ToString();
+            }
+        }
 
-		public String AssemblyProduct
-		{
-			get
-			{
-				Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+        public String AssemblyProduct
+        {
+            get
+            {
+                Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyProductAttribute), false);
 
-				if (attributes.Length == 0)
-				{
-					return String.Empty;
-				}
+                if (attributes.Length == 0)
+                {
+                    return String.Empty;
+                }
 
-				return ((AssemblyProductAttribute)attributes[0]).Product;
-			}
-		}
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
 
-		public String AssemblyCompany
-		{
-			get
-			{
-				Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+        public String AssemblyCompany
+        {
+            get
+            {
+                Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
 
-				if (attributes.Length == 0)
-				{
-					return String.Empty;
-				}
+                if (attributes.Length == 0)
+                {
+                    return String.Empty;
+                }
 
-				return ((AssemblyCompanyAttribute)attributes[0]).Company;
-			}
-		}
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
 
-		public String AssemblyCopyright
-		{
-			get
-			{
-				Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+        public String AssemblyCopyright
+        {
+            get
+            {
+                Object[] attributes = this.AssemblyObject.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
 
-				if (attributes.Length == 0)
-				{
-					return String.Empty;
-				}
+                if (attributes.Length == 0)
+                {
+                    return String.Empty;
+                }
 
-				return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-			}
-		}
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
