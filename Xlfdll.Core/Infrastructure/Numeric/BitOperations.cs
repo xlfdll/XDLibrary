2280930db305ba1e7fa73f9098a01df0be1b0ca9
xlfdll <xlfdll@gmail.com>
2019-01-27 20:11:42 -0800

    Added numeric operation classes

---
+++ Xlfdll.Core/Infrastructure/Numeric/BitOperations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Xlfdll
+{
+    public static class BitOperations
+    {
+        public static UInt16 GetBit(this UInt16 n, Int32 index)
+        {
+            return (UInt16)GetBit((UInt32)n, index);
+        }
+
+        public static UInt16 SetBit(this UInt16 n, Int32 index)
+        {
+            return (UInt16)SetBit((UInt32)n, index);
+        }
+
+        public static UInt16 ClearBit(this UInt16 n, Int32 index)
+        {
+            return (UInt16)ClearBit((UInt32)n, index);
+        }
+
+        public static UInt32 GetBit(this UInt32 n, Int32 index)
+        {
+            return (n >> index) & 1U;
+        }
+
+        public static UInt32 SetBit(this UInt32 n, Int32 index)
+        {
+            return n | (1U << index);
+        }
+
+        public static UInt32 ClearBit(this UInt32 n, Int32 index)
+        {
+            return n & ~(1U << index);
+        }
+
+        public static UInt64 GetBit(this UInt64 n, Int32 index)
+        {
+            return (n >> index) & 1UL;
+        }
+
+        public static UInt64 SetBit(this UInt64 n, Int32 index)
+        {
+            return n | (1UL << index);
+        }
+
+        public static UInt64 ClearBit(this UInt64 n, Int32 index)
+        {
+            return n & ~(1UL << index);
+        }
+    }
+}
---
+++ Xlfdll.Core/Infrastructure/Numeric/IntegerConversions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Xlfdll
+{
+    public static class IntegerConversions
+    {
+        public static String ToPaddedHexString(this UInt64 n, Boolean includePrefix)
+        {
+            return (includePrefix ? "0x" : String.Empty) + n.ToString("X16");
+        }
+
+        public static String ToPaddedHexString(this UInt32 n, Boolean includePrefix)
+        {
+            return (includePrefix ? "0x" : String.Empty) + n.ToString("X8");
+        }
+
+        public static String ToPaddedHexString(this UInt16 n, Boolean includePrefix)
+        {
+            return (includePrefix ? "0x" : String.Empty) + n.ToString("X4");
+        }
+
+        public static String ToPaddedHexString(this Byte n, Boolean includePrefix)
+        {
+            return (includePrefix ? "0x" : String.Empty) + n.ToString("X2");
+        }
+
+        public static UInt64 ConvertHexStringToUInt64(String hexString)
+        {
+            return Convert.ToUInt64(hexString, 16);
+        }
+
+        public static UInt32 ConvertHexStringToUInt32(String hexString)
+        {
+            return Convert.ToUInt32(hexString, 16);
+        }
+
+        public static UInt16 ConvertHexStringToUInt16(String hexString)
+        {
+            return Convert.ToUInt16(hexString, 16);
+        }
+
+        public static Byte ConvertHexStringToByte(String hexString)
+        {
+            return Convert.ToByte(hexString, 16);
+        }
+    }
+}
