ebab909aba0bd5fb6802a2d9cc08e589abfe3230
xlfdll <xlfdll@gmail.com>
2019-01-27 20:24:32 -0800

    Added logging support classes

---
+++ Xlfdll.Core/Logging/LoggingExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace Xlfdll.Logging
+{
+    public static class LoggingExtensions
+    {
+        public static String GetLogContents(this Exception ex)
+        {
+            StringBuilder sb = new StringBuilder();
+            Exception currentEx = ex;
+            Int32 i = 0;
+
+            while (currentEx != null)
+            {
+                i++;
+
+                sb.AppendLine("////////// Level " + i.ToString() + " //////////");
+                sb.AppendLine();
+
+                sb.AppendLine("Type: ");
+                sb.AppendLine("-----");
+                sb.AppendLine(currentEx.GetType().FullName);
+                sb.AppendLine();
+
+                sb.AppendLine("Module: ");
+                sb.AppendLine("-------");
+                sb.AppendLine(currentEx.Source);
+                sb.AppendLine();
+
+                sb.AppendLine("Method: ");
+                sb.AppendLine("-------");
+                sb.AppendLine(currentEx.TargetSite.ToString());
+                sb.AppendLine();
+
+                sb.AppendLine("Message:");
+                sb.AppendLine("--------");
+                sb.AppendLine(currentEx.Message);
+                sb.AppendLine();
+
+                sb.AppendLine("Stack Trace:");
+                sb.AppendLine("------------");
+                sb.AppendLine(currentEx.StackTrace);
+                sb.AppendLine();
+
+                currentEx = currentEx.InnerException;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Xlfdll.Core/Logging/QueuedLogger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xlfdll.Text;
+
+namespace Xlfdll.Logging
+{
+    public class QueuedLogger
+    {
+        public QueuedLogger(String fileName)
+        {
+            this.FileName = fileName;
+
+            this.LogQueue = new ConcurrentQueue<String>();
+            this.CancellationTokenSource = new CancellationTokenSource();
+            this.LogTask = new Task(ProcessLogs, this.CancellationTokenSource.Token);
+        }
+
+        public String FileName { get; }
+
+        public void Start()
+        {
+            this.LogTask.Start();
+        }
+
+        public void Stop()
+        {
+            this.CancellationTokenSource.Cancel();
+
+            while (!this.LogTask.IsCompleted) { }
+
+            // No need to dispose Task objects
+            // See https://stackoverflow.com/questions/3734280/is-it-considered-acceptable-to-not-call-dispose-on-a-tpl-task-object
+        }
+
+        public void WriteLog(String log)
+        {
+            this.LogQueue.Enqueue(log);
+        }
+
+        private void ProcessLogs()
+        {
+            using (StreamWriter writer = new StreamWriter(this.FileName, true, AdditionalEncodings.UTF8WithoutBOM)
+            { AutoFlush = true })
+            {
+                String line;
+
+                while (!this.CancellationTokenSource.IsCancellationRequested)
+                {
+                    while (this.LogQueue.TryDequeue(out line))
+                    {
+                        writer.WriteLine(line);
+                    }
+
+                    Thread.Sleep(500);
+                }
+
+                // After cancellation, process the remaining log messages
+                while (this.LogQueue.TryDequeue(out line))
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        private ConcurrentQueue<String> LogQueue { get; }
+        private Task LogTask { get; }
+        private CancellationTokenSource CancellationTokenSource { get; }
+    }
+}
