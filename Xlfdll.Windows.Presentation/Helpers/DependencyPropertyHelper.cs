05fa1a610ddbaec155a976cf6da25b82bff77540
xlfdll <xlfdll@gmail.com>
2019-06-12 10:18:56 -0700

    Added DependencyPropertyHelper class

---
+++ Xlfdll.Windows.Presentation/Helpers/DependencyPropertyHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace Xlfdll.Windows.Presentation
+{
+    public static class DependencyPropertyHelper
+    {
+        public static DependencyProperty Create<PropertyType, OwnerType>(String propertyName, PropertyType defaultValue = default(PropertyType))
+        {
+            return DependencyProperty.Register(propertyName,
+                typeof(PropertyType), typeof(OwnerType),
+                new FrameworkPropertyMetadata(defaultValue));
+        }
+    }
+}
