c6e5a3c2fd53ea7ed12c6480eecf1bf18454e852
xlfdll <xlfdll@gmail.com>
2019-06-12 10:46:29 -0700

    Added CreateAttached method in DependencyPropertyHelper class

--- Xlfdll.Windows.Presentation/Helpers/DependencyPropertyHelper.cs
+++ Xlfdll.Windows.Presentation/Helpers/DependencyPropertyHelper.cs
@@ -5,11 +5,21 @@ namespace Xlfdll.Windows.Presentation
 {
     public static class DependencyPropertyHelper
     {
-        public static DependencyProperty Create<PropertyType, OwnerType>(String propertyName, PropertyType defaultValue = default(PropertyType))
+        public static DependencyProperty Create<PropertyType, OwnerType>(String propertyName,
+            PropertyType defaultValue = default(PropertyType))
         {
             return DependencyProperty.Register(propertyName,
                 typeof(PropertyType), typeof(OwnerType),
                 new FrameworkPropertyMetadata(defaultValue));
+        }
+
+        public static DependencyProperty CreateAttached<PropertyType, OwnerType>(String propertyName,
+            PropertyChangedCallback propertyChangedCallback,
+            PropertyType defaultValue = default(PropertyType))
+        {
+            return DependencyProperty.RegisterAttached(propertyName,
+                typeof(PropertyType), typeof(OwnerType),
+                new FrameworkPropertyMetadata(defaultValue, propertyChangedCallback));
         }
     }
 }
