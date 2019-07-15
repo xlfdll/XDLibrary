b30c4f4c46a55dabb0a4a727ef0f3b7002cbcc19
xlfdll <xlfdll@gmail.com>
2019-07-14 23:50:26 -0700

    Xlfdll.Core: Added LocalizationExtensions class

---
+++ Xlfdll.Core/Localization/LocalizationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xlfdll.Localization;
+
+namespace Xlfdll.Core.Localization
+{
+    public static class LocalizationExtensions
+    {
+        public static String GetTranslatedString(this LanguageDictionary languageDictionary, String path, String fallbackText)
+        {
+            String[] segments = path.Trim().Split('.');
+
+            return languageDictionary.GetTranslatedString(segments[0], segments[1], fallbackText);
+        }
+
+        public static String GetTranslatedString(this LanguageDictionary languageDictionary, String sectionName, String keyName, String fallbackText)
+        {
+            return (languageDictionary != null
+                && languageDictionary.ContainsSection(sectionName)
+                && languageDictionary[sectionName].ContainsKey(keyName))
+                ? languageDictionary[sectionName][keyName]
+                : fallbackText;
+        }
+    }
+}
