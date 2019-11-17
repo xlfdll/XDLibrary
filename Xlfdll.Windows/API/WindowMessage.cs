37973eb0097bbe4d525ab0f243f512741c1ffcea
xlfdll <xlfdll@gmail.com>
2019-11-17 02:57:10 -0800

    Xlfdll.Windows: Added WM_CHANGECBCHAIN message with SendMessage P/Invoke method

--- Xlfdll.Windows/API/WindowMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Xlfdll.Windows.API
-{
-    public static class WindowMessage
-    {
-        public const Int32 WM_DRAWCLIPBOARD = 0x0308;
-    }
-}
---
+++ Xlfdll.Windows/API/WindowMessages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Xlfdll.Windows.API
+{
+    public static class WindowMessages
+    {
+        #region Functions
+
+        //[Out] StringBuilder: require initialization of the string builder with proper length first.
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, StringBuilder lParam);
+
+        #endregion
+
+        #region Constants
+
+        public const Int32 WM_DRAWCLIPBOARD = 0x0308;
+        public const Int32 WM_CHANGECBCHAIN = 0x030D;
+
+        #endregion
+    }
+}
