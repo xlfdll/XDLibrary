ef0b2f401eaf9e82e7980262148a7d20f1a855a0
xlfdll <xlfdll@gmail.com>
2019-11-17 02:38:32 -0800

    Xlfdll.Windows: Added Windows API P/Invoke methods for clipboard monitoring

---
+++ Xlfdll.Windows/API/DataExchange.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Xlfdll.Windows.API
+{
+    public static class DataExchange
+    {
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+    }
+}
---
+++ Xlfdll.Windows/API/WindowMessage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Xlfdll.Windows.API
+{
+    public static class WindowMessage
+    {
+        public const Int32 WM_DRAWCLIPBOARD = 0x0308;
+    }
+}
