37f7ea40fe2c567d2eadd5458c741fc542358c17
xlfdll <xlfdll@gmail.com>
2020-01-03 22:02:11 -0800

    Xlfdll.Core: Added SpecialFolders class for Program Files folder paths

---
+++ Xlfdll.Core/Infrastructure/Environment/SpecialFolders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xlfdll
+{
+    public static class SpecialFolders
+    {
+        public static String ProgramFilesInX86
+            => !Environment.Is64BitOperatingSystem
+            ? Environment.GetEnvironmentVariable("ProgramFiles")
+            : Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+
+        public static String ProgramFilesInX64
+            => Environment.GetEnvironmentVariable("ProgramW6432");
+    }
+}
