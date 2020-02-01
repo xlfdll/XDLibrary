5fc54cd95ec2349519ccdc4ad9b16c4c374d9719
xlfdll <xlfdll@gmail.com>
2020-01-31 23:45:17 -0800

    Xlfdll.Core: Added PathExtensions class

---
+++ Xlfdll.Core/IO/PathExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Xlfdll.IO
+{
+    public static class PathExtensions
+    {
+        public static String GetSafeFileName(String fileName)
+        {
+            Char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
+
+            StringBuilder sb = new StringBuilder(fileName);
+
+            foreach (Char c in invalidFileNameChars)
+            {
+                sb.Replace(c, '_');
+            }
+
+            return sb.ToString();
+        }
+
+        public static String GetSafePath(String path)
+        {
+            Char[] invalidPathChars = Path.GetInvalidPathChars();
+
+            StringBuilder sb = new StringBuilder(path);
+
+            foreach (Char c in invalidPathChars)
+            {
+                sb.Replace(c, '_');
+            }
+
+            return sb.ToString();
+        }
+    }
+}
