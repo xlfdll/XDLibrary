545f124af4cef364753cd1cd9f907074af069098
xlfdll <xlfdll@gmail.com>
2020-06-07 21:34:42 -0700

    Added FilePathToFileNameConverter class in Xlfdll.Windows.Presentation project

---
+++ Xlfdll.Windows.Presentation/Converters/FilePathToFileNameConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Xlfdll.Windows.Presentation
+{
+    public class FilePathToFileNameConverter : MarkupExtension, IValueConverter
+    {
+        public override Object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            String filePath = value as String;
+
+            if (!String.IsNullOrEmpty(filePath))
+            {
+                return Path.GetFileName(filePath);
+            }
+
+            return null;
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
