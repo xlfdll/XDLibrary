fc065cdeccb5d323144be8cbb357b6dd7ccf02f1
xlfdll <xlfdll@gmail.com>
2020-06-09 06:47:18 -0700

    Xlfdll.Core: Added ExceptionEventArgs class

---
+++ Xlfdll.Core/Infrastructure/Events/ExceptionEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xlfdll
+{
+    public class ExceptionEventArgs : EventArgs
+    {
+        public ExceptionEventArgs(Exception ex)
+            : base()
+        {
+            this.Exception = ex;
+        }
+
+        public Exception Exception { get; }
+    }
+}
