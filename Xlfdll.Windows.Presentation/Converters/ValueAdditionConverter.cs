9af349a001872311a822735bdf15c5747cc961cd
xlfdll <xlfdll@gmail.com>
2020-06-10 23:05:19 -0700

    Xlfdll.Windows.Presentation: Added ValueAdditionConverter and ValueSubtractionConverter classes

---
+++ Xlfdll.Windows.Presentation/Converters/ValueAdditionConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Xlfdll.Windows.Presentation
+{
+    public class ValueAdditionConverter : MarkupExtension, IValueConverter
+    {
+        public override Object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            return (Double)value + (Double)parameter;
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
