5a102ba1f290f0de03b4f564cea0ec732a019094
xlfdll <xlfdll@gmail.com>
2020-09-01 10:33:55 -0700

    Xlfdll.Core: Added exception handler in TextLanguagePackProcessor to provide clear explanation on invalid line in language packs

--- Xlfdll.Core/Localization/TextLanguagePackProcessor.cs
+++ Xlfdll.Core/Localization/TextLanguagePackProcessor.cs
@@ -12,25 +12,32 @@ namespace Xlfdll.Localization
 
             using (StreamReader reader = new StreamReader(path, Encoding.UTF8))
             {
-                string line = String.Empty;
+                String line = String.Empty;
                 String currentSectionName = String.Empty;
 
                 while (!reader.EndOfStream)
                 {
                     line = reader.ReadLine();
 
-                    if (line.StartsWith("Language"))
+                    try
                     {
-                        languageDictionary = new LanguageDictionary(line.Substring(line.IndexOf('=') + 1).Trim());
+                        if (line.StartsWith("Language"))
+                        {
+                            languageDictionary = new LanguageDictionary(line.Substring(line.IndexOf('=') + 1).Trim());
+                        }
+                        else if (line.StartsWith("[") && !line.Equals("[Metadata]"))
+                        {
+                            currentSectionName = line.Substring(line.IndexOf('[') + 1, line.IndexOf(']') - line.IndexOf('[') - 1);
+                            languageDictionary.AddSection(currentSectionName);
+                        }
+                        else if (line.StartsWith("$"))
+                        {
+                            languageDictionary[currentSectionName].Add(line.Substring(line.IndexOf('$') + 1, line.IndexOf('=') - line.IndexOf('$') - 1).Trim(), line.Substring(line.IndexOf('=') + 1).Trim());
+                        }
                     }
-                    else if (line.StartsWith("[") && !line.Equals("[Metadata]"))
+                    catch (Exception ex)
                     {
-                        currentSectionName = line.Substring(line.IndexOf('[') + 1, line.IndexOf(']') - line.IndexOf('[') - 1);
-                        languageDictionary.AddSection(currentSectionName);
-                    }
-                    else if (line.StartsWith("$"))
-                    {
-                        languageDictionary[currentSectionName].Add(line.Substring(line.IndexOf('$') + 1, line.IndexOf('=') - line.IndexOf('$') - 1).Trim(), line.Substring(line.IndexOf('=') + 1).Trim());
+                        throw new InvalidDataException("Invalid format on line: " + line, ex);
                     }
                 }
             }
