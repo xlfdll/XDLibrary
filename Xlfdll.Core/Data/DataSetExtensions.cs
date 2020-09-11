201de0401a6a0434918de992c1f2b8df3a4baacf
xlfdll <xlfdll@gmail.com>
2020-09-11 10:03:42 -0700

    Xlfdll.Core: Added DataSetExtensions class

---
+++ Xlfdll.Core/Data/DataSetExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Xlfdll.Data
+{
+    public static class DataSetExtensions
+    {
+        public static String ToCSV(this DataTable dataTable)
+        {
+            StringBuilder sb = new StringBuilder();
+            IEnumerable<String> columnNames = dataTable.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName);
+
+            sb.AppendLine(String.Join(",", columnNames));
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                IEnumerable<String> fields = row.ItemArray.Select(field => field.ToString());
+
+                sb.AppendLine(String.Join(",", fields));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
