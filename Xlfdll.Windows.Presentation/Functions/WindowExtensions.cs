524f0614ff5b76a851b9fd065ce1428ef6b9817c
xlfdll <xlfdll@gmail.com>
2021-03-02 22:36:56 -0800

    Xlfdll.Windows.Presentation: Added center window function extension

--- Xlfdll.Windows.Presentation/Functions/WindowExtensions.cs
+++ Xlfdll.Windows.Presentation/Functions/WindowExtensions.cs
@@ -7,7 +7,18 @@ using Xlfdll.Windows.API;
 namespace Xlfdll.Windows.Presentation
 {
     public static class WindowExtensions
-	{
+    {
+        public static void CenterWindowToScreen(this Window window)
+        {
+            Double screenWidth = SystemParameters.PrimaryScreenWidth;
+            Double screenHeight = SystemParameters.PrimaryScreenHeight;
+            Double windowWidth = window.Width;
+            Double windowHeight = window.Height;
+
+            window.Left = (screenWidth / 2) - (windowWidth / 2);
+            window.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
+
         public static void EnableMinimizeBox(this Window window)
         {
             WindowInteropHelper windowInteropHelper = new WindowInteropHelper(window);
