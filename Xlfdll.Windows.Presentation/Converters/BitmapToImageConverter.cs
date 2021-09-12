8b0c3e5daa33c7b576717b716ef318ebfd46d872
xlfdll <xlfdll@gmail.com>
2021-09-11 17:37:20 -0700

    Xlfdll.Windows.Presentation: Added helper and converter classes for converting GDI+ Bitmap objects to BitmapImage

---
+++ Xlfdll.Windows.Presentation/Converters/BitmapToImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Xlfdll.Windows.Presentation
+{
+    public class BitmapToImageConverter : MarkupExtension, IValueConverter
+    {
+        public override Object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            if (value is Bitmap bitmap)
+            {
+                return bitmap.ConvertToImage();
+            }
+
+            return null;
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Xlfdll.Windows.Presentation/Helpers/InteropHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace Xlfdll.Windows.Presentation
+{
+    public static class InteropHelper
+    {
+        public static BitmapImage ConvertToImage(this Bitmap bitmap)
+        {
+            BitmapImage bitmapimage = new BitmapImage();
+
+            using (MemoryStream memory = new MemoryStream()
+            {
+                Position = 0
+            })
+            {
+                bitmap.Save(memory, ImageFormat.Bmp);
+
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+            }
+
+            return bitmapimage;
+        }
+    }
+}
