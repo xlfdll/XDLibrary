8b8c49e065b03204bd974e28b1a5daf38fdd9d86
xlfdll <xlfdll@gmail.com>
2021-09-26 11:58:07 -0700

    Xlfdll.Core: Added new properties for getting Program Files folder path based on OS and process modes

--- Xlfdll.Core/Infrastructure/Environment/SpecialFolders.cs
+++ Xlfdll.Core/Infrastructure/Environment/SpecialFolders.cs
@@ -4,6 +4,16 @@ namespace Xlfdll
 {
     public static class SpecialFolders
     {
+        public static String ProgramFilesBasedOnOS
+            => Environment.Is64BitOperatingSystem
+            ? SpecialFolders.ProgramFilesInX64
+            : SpecialFolders.ProgramFilesInX86;
+
+        public static String ProgramFilesBasedOnProcess
+            => Environment.Is64BitProcess
+            ? SpecialFolders.ProgramFilesInX64
+            : SpecialFolders.ProgramFilesInX86;
+
         public static String ProgramFilesInX86
             => !Environment.Is64BitOperatingSystem
             ? Environment.GetEnvironmentVariable("ProgramFiles")
