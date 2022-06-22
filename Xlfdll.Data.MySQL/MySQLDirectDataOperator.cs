cb4ae1331eeeca1181760719e941db9b297e3c8c
xlfdll <xlfdll@gmail.com>
2022-06-22 22:43:25 +0900

    Xlfdll.Data.MySQL: Replaced NuGet package for MySQL connector

--- Xlfdll.Data.MySQL/MySQLDirectDataOperator.cs
+++ Xlfdll.Data.MySQL/MySQLDirectDataOperator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 
-using MySql.Data.MySqlClient;
+using MySqlConnector;
 
 namespace Xlfdll.Data.MySQL
 {
--- Xlfdll.Data.MySQL/MySQLQueuedDataOperator.cs
+++ Xlfdll.Data.MySQL/MySQLQueuedDataOperator.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-using MySql.Data.MySqlClient;
+using MySqlConnector;
 
 namespace Xlfdll.Data.MySQL
 {
