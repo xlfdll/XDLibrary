4a4afd1f0404ed2c7ccd6a8434dc15fa3cc49055
xlfdll <xlfdll@gmail.com>
2022-12-30 01:25:25 +0900

    Xlfdll.Core: Added NotifiableObservableCollection class

---
+++ Xlfdll.Core/Infrastructure/Collections/NotifiableObservableCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Xlfdll.Collections
+{
+    public class NotifiableObservableCollection<T> : ObservableCollection<T>
+    {
+        public void Refresh()
+        {
+            for (Int32 i = 0; i < this.Count; i++)
+            {
+                this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            }
+        }
+    }
+}
